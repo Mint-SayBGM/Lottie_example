0131c60a755eb1bf597a17907a09de926d0a58cd
GwangMinBack <bgm8862@gmail.com>
2018-05-24 11:17:27 +0900

    new animation2 add

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var animate = lottie.loadAnimation({
   container: document.getElementById("hello"),
   renderer: 'svg',
-  loop: true,
   autoplay: true,
-  path: 'data.json'
+  loop:true,
+  path: 'animation2.json'
 })
-animate.playSegments([[0,25], [30,90]],true);
